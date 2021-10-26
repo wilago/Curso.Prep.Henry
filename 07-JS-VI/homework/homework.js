04a580de8332b5ffcf03c6fadaaf89103a99a189
Wilson Lasso <wilago1979@gmail.com>
2021-10-26 15:29:04 -0500

    HomeWork 07-JS-VI_26_10_2021

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -59,7 +59,11 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var nuevoArray= [];
 
+ 
+    nuevoArray.push(array.map(cb(array[i])));
+}
 
 
 
@@ -69,6 +73,18 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+
+    var dataString= "";
+    var nuevoArray;
+
+  for (let i = 0; i < array.length; i++) {
+    dataString=array[i];
+
+          if(dataString.charAt(0)=="a"){
+              nuevoArray.push(dataString);
+          }
+  }
+    return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea
